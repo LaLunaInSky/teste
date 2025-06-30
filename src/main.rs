e739c4100383107ea1018ddc9f76ada333ac8856
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-29 22:34:57 -0300

    29/06/2025 22:34

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use leptos::{
+    prelude::*,
+    mount::mount_to_body
+};
+
+#[component]
+fn App() -> impl IntoView {
+    view! {
+        <main>
+            <h1>
+                "Teste"
+            </h1>
+        </main>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    mount_to_body(App);
 }
